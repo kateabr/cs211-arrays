608e87aec993a462c0d4506acd93a4df0f2a4932
kateabr <kateabr@yandex.ru>
2015-10-09 00:04:32 +0400

    Пятое задание

--- Bonus-3/arrays.h
+++ Bonus-3/arrays.h
@@ -5,5 +5,6 @@ void task_1();
 void task_2();
 int task_3_zero();
 void task_4();
+void task_5_copy(int * arr1, int * arr2, int size);
 
 #endif
--- Bonus-3/arrays_main.cpp
+++ Bonus-3/arrays_main.cpp
@@ -9,4 +9,10 @@ int main()
 	task_2();
 	assert(task_3_zero() == 0);
 	task_4();
+	const int size = 4;
+	int arr1[size]{ 1,2,3,4 };
+	int arr2[size];
+	task_5_copy(arr1, arr2, size);
+	for (int i = 0; i < size; i++)
+		assert(arr1[i] == arr2[i]);
 }
